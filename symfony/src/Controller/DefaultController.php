625948ba266e0184cd0fb6c69922aad457432898
capons <bogdan.dvinin@rambler.ru>
2018-02-05 21:27:57 +0200

    f

---
+++ symfony/src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index()
+    {
+      
+
+        return $this->render('base.html.twig', [
+            'posts' => [],
+        ]);
+    }
+}
